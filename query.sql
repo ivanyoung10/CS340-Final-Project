40e1b7acac202a7eac7b69f10a165360745d9da4
ivanyoung10 <129990045+ivanyoung10@users.noreply.github.com>
2024-02-27 09:43:04 -0800

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE wsig
+
+CREATE TABLE  (
+    column-name data-type1 WITH DEFAULT constant,
+    column-name data-type2 NOT NULL
+)
+    IN database-name.table-space-name
+    CCSID ccsid-value
+    BUFFERPOOL bpname
